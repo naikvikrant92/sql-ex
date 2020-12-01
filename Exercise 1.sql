f3987ab79fb0abb4430e19cbaeb2f12003252bc3
naikvikrant92 <cr9vikrant@gmail.com>
2020-12-01 18:55:57 +0530

    Add files via upload

---
+++ Exercise 1.sql
@@ -0,0 +1,6 @@
+/*
+Exercise: 1 (Serge I: 2002-09-30)
+Find the model number, speed and hard drive capacity for all the PCs with prices below $500.
+Result set: model, speed, hd.*/
+
+SELECT MODEL,SPEED,HD FROM PC WHERE PRICE<500
